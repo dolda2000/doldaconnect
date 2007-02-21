1a893343f388e2bde1ba33dfea9e8f5a48a2f70b
fredrik <fredrik@959494ce-11ee-0310-bf91-de5d638817bd>
2007-02-21 02:52:59 +0000

    Fix typo.
    git-svn-id: svn+ssh://svn.dolda2000.com/srv/svn/repos/src/doldaconnect@845 959494ce-11ee-0310-bf91-de5d638817bd

--- lib/guile/dolcon/util.scm
+++ lib/guile/dolcon/util.scm
@@ -64,10 +64,10 @@
 				   (begin (fn-updattr (car ires) a (cadr ires))
 					  (notify a (cdr (assq (car ires) fnetnodes)))))))))
       (dc-loop-reg ".notify" 601 (lambda (r er) (let* ((ires (dc-intresp r))
-						       (hubform (assq (car ires) fnetnode)))
+						       (hubform (assq (car ires) fnetnodes)))
 						  (if hubform
 						      (begin (fn-updattr (car ires) 'state (list-ref '(syn hs est dead) (cadr ires)))
-							     (notify 'state (cdr (assq (car ires) fnetnodes))))))))
+							     (notify 'state (cdr hubform)))))))
       (dc-loop-reg ".notify" 602 (lambda (r er) (ua r 'name)))
       (dc-loop-reg ".notify" 605 (lambda (r er) (ua r 'users)))
       (dc-loop-reg ".notify" 604 (lambda (r er)
